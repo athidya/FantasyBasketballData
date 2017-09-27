fbd2a049bcf700672169e93e72f3abf80cf8406f
Athidya <athidyaraveenthranehru@gmail.com>
2017-09-26 23:18:16 -0400

    oauth library added, login activity added

--- app/src/main/java/com/example/athidya/mydataapp/MainActivityStart.java
+++ app/src/main/java/com/example/athidya/mydataapp/MainActivityStart.java
@@ -5,16 +5,49 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.*;
-import com.android.volley.toolbox.*;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import oauth.signpost.OAuth;
+import oauth.signpost.*;
+import org.apache.*;
 
 public class MainActivityStart extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client2;
+
+
+
+    private static final String REQUEST_TOKEN_ENDPOINT_URL = "https://api.login.yahoo.com/oauth/v2/get_request_token";
+    private static final String ACCESS_TOKEN_ENDPOINT_URL = "https://api.login.yahoo.com/oauth/v2/get_access_token";
+    private static final String AUTHORIZE_WEBSITE_URL = "https://api.login.yahoo.com/oauth/v2/request_auth";
+    private static final int PIN_DIALOG = 0;
+    String CALLBACK_URL = OAuth.OUT_OF_BAND; //check android manifest for scheme and host if error
+
+    String CONSUMER_KEY = "";
+    String CONSUMER_SECRET = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +65,49 @@ public class MainActivityStart extends AppCompatActivity {
             }
         });
 
+
         Button button = (Button) findViewById(R.id.button);
         final TextView mTextView = (TextView) findViewById(R.id.textView2);
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                /*Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(login);*/
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "";
+                // Request a string response from the provided URL.
+                /*JsonObjectRequest signin = new JsonObjectRequest(Request.Method.GET.url,
+                        new Response.Listener<JsonObjectRequest>(){
+
+                        })*/
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                mTextView.setText("Response is: "+ response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mTextView.setText("That didn't work!");
+                        error.printStackTrace();
+                    }
+                });
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+            }
+        });
+
+
+        /* button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                String url ="https://androidtutorialpoint.com/api/volleyString";
+                String url ="https://fantasysports.yahooapis.com/fantasy/v2/game/nfl";
 
                 // Request a string response from the provided URL.
                 StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -60,7 +127,9 @@ public class MainActivityStart extends AppCompatActivity {
                 // Add the request to the RequestQueue.
                 queue.add(stringRequest);
             }
-        });
+        });*/
+
+
     }
 
 
